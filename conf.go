7b77202326fe2976e60707715dbbd30a59cd1646
zkfy <lib4net@163.com>
2021-01-13 16:41:22 +0800

    重构代码

--- conf.go
+++ conf.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"github.com/micro-plat/hydra"
-	"github.com/micro-plat/rlog/services/log"
+	"github.com/micro-plat/rlog/services"
 )
 
 //init 检查应用程序配置文件，并根据配置初始化服务
 func init() {
 
-	App.Micro("/log/save", log.NewSaveHandler) //根据配置的日志名称，初始化服务
+	App.Micro("/log/save", services.SaveHandle) //根据配置的日志名称，初始化服务
 
 	hydra.OnReady(func() {
 
--- go.mod
+++ go.mod
@@ -3,10 +3,8 @@ module github.com/micro-plat/rlog
 go 1.15
 
 require (
-	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef
 	github.com/micro-plat/hydra v1.0.5
 	github.com/micro-plat/lib4go v1.0.10
-	github.com/zkfy/cron v1.0.0
 	gopkg.in/olivere/elastic.v5 v5.0.86
 )
 
